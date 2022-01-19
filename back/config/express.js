2b24ce4a60c8b988c00098a5d5af3cfb58d4af58
jiwoon <mmvv11@naver.com>
2022-01-19 21:23:20 +0900

    edit

--- back/config/express.js
+++ back/config/express.js
@@ -15,6 +15,8 @@ module.exports = function () {
   app.use(methodOverride());
 
   app.use(cors());
+  app.use(express.static("/home/ubuntu/food-map/front"));
+
   // app.use(express.static(process.cwd() + '/public'));
 
   /* App (Android, iOS) */
